fa037180b829c42caf8d6295054e4f032abd42be
marijose <guerreromjosefa@gmail>
2023-01-14 12:12:15 +0100

    first commit

---
+++ src/pages/form.tsx
@@ -0,0 +1,19 @@
+const Form = () => {
+    return(
+        <div>
+            <form action="">
+                <label htmlFor="">
+                    List name
+                    <input type="text" />
+                </label>
+                <label htmlFor="">
+                    Item list
+                    <input type="text" />
+                </label>
+            </form>
+            <button>Save</button>
+        </div>
+    )
+}
+
+export default Form;
---
+++ src/pages/home.tsx
@@ -0,0 +1,22 @@
+import Form from './form';
+
+const Home = () => {
+    return(
+        <div>
+            <header>
+                <h2>list App</h2>
+            </header>
+            <section>
+                <div>
+                    <p>how to use?</p>
+                    <p>functional app description..... </p>
+                </div>
+                <button /*onClick={ () => .<Form /> }*/>create new list</button>
+                <button>lists menu</button>
+            </section>
+
+        </div>
+    )
+}
+
+export default Home;
